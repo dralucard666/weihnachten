8b1beb399beb53853529a7fd741115ef569f08c7
Jonas Brossmann <jbrossmann@coconut-capital.com>
2025-11-30 23:46:19 +0100

    start script

---
+++ start-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copy docker-compose configuration
+cp docker-compose.yml.old docker-compose.yml
+
+# Rebuild and restart containers
+docker-compose down && docker-compose up -d --build
